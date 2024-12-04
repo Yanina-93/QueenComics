5eae65d29b40c85b90639a2033b407dba93789fd
LinhLe9 <135984977+LinhLe9@users.noreply.github.com>
2024-12-04 17:56:43 +0000

    Delete js/scripts.js

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.new-article-form').addEventListener('submit', function (e) {
-        e.preventDefault();
-        const email = e.target.querySelector('input[type="email"]').value;
-		console.log(email);
-        alert(`Thank you for subscribing with email: ${email}`);
-    });
